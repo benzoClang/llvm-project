8168c582ef7f295a8a6b139fdd4170945dba5966
Chih-Hung Hsieh <chh@google.com>
2019-06-03 11:10:52 -0700

    Revert two changes that break Android builds
    * Earlier versions of lld were able to replace Android's
      lld.gold and link most files.
    * With two recent lld changes, debuggerd_test64 and some other files
      failed to link because libunwindstack.a is linked like --whole-archive.
      We need to revert the following two changes:
      * "Put undefined symbols from shared libraries in the symbol table."
         lld/trunk@326242
      * "Make undefined symbol in DSO to pull out object files from archive files."
         lld/trunk@325849
    
    Bug: 74755833
    Change-Id: I18a59ec2e4d13b6698b75ed3e4ac6de290431779
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- lld/ELF/InputFiles.cpp
+++ lld/ELF/InputFiles.cpp
@@ -1327,9 +1327,7 @@ template <class ELFT> void SharedFile::parse() {
     }
 
     if (sym.isUndefined()) {
-      Symbol *s = symtab->addSymbol(
-          Undefined{this, name, sym.getBinding(), sym.st_other, sym.getType()});
-      s->exportDynamic = true;
+      this->Undefs.insert(name);
       continue;
     }
 
